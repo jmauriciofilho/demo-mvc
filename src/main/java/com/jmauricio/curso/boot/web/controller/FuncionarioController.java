093d6c784d5c77835f00353c739a3a6e4b0cbf62
Mauricio Pereira <josemauriciobpfilho@gmail.com>
2020-08-30 19:08:40 -0300

    first commit

---
+++ src/main/java/com/jmauricio/curso/boot/web/controller/FuncionarioController.java
@@ -0,0 +1,20 @@
+package com.jmauricio.curso.boot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/funcionarios")
+public class FuncionarioController {
+
+    @GetMapping("/cadastrar")
+    public String Cadastrar(){
+        return "/funcionario/cadastro";
+    }
+
+    @GetMapping("/listar")
+    public String Listar(){
+        return "/funcionario/lista";
+    }
+}
