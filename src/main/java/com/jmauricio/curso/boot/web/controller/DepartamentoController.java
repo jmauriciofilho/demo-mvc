40ddffdf1569dfbf769c61797e439104da38ae2b
Mauricio Pereira <jose.mauricio@catskillet.com>
2020-09-05 17:18:42 -0300

    listar

--- src/main/java/com/jmauricio/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/jmauricio/curso/boot/web/controller/DepartamentoController.java
@@ -4,7 +4,9 @@ import com.jmauricio.curso.boot.domain.Departamento;
 import com.jmauricio.curso.boot.service.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,13 +23,26 @@ public class DepartamentoController {
     }
 
     @GetMapping("/listar")
-    public String Listar(){
+    public String Listar(ModelMap model){
+        model.addAttribute("departamentos", service.buscarTodos());
         return "/departamento/lista";
     }
 
     @PostMapping("/salvar")
     public String salvar(Departamento departamento){
         service.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable Long id, ModelMap model){
+        model.addAttribute("departamento", service.buscarPorId(id));
+        return "/departamento/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(Departamento departamento){
+        service.editar(departamento);
         return "redirect:/departamentos/cadastrar";
     }
 }
